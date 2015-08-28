98c25e668812657ccb559f98d6b25f3bc6c5755a
nicbrant <nicbrant@gmail.com>
2015-08-27 23:30:39 -0300

    Definindo as Structs

--- listas.c
+++ listas.c
@@ -1,6 +1,17 @@
-/* Nicholas Brant - 140157158
-** 
-**
-**
-**
+/* Programação Sistematica - Turma A - Professora: Genaina Nunes Rodrigues
+** Nicholas Brant - 140157158
 */
+struct nodo{
+    struct nodo* prox;
+    struct nodo* ante;
+    char nome[15];
+    char genero;
+    char estado[10];
+    unsigned short int idade;
+}typedef Nodo;
+
+struct list{
+    Nodo* Atual;
+    Nodo* Primeiro;
+    Nodo* Ultimo;
+}typedef List;
