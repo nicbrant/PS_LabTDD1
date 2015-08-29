b790a34e5a15bc095f00ad8b1ddf334c54a44050
nicecookie <rafaelmartinsrm@gmail.com>
2015-08-29 14:14:04 -0300

    Added my name and renamed variables

--- listas.c
+++ listas.c
@@ -1,17 +1,22 @@
-/* Programação Sistematica - Turma A - Professora: Genaina Nunes Rodrigues
-** Nicholas Brant - 140157158
+/* Programação Sistematica - Turma A - Professora: Genaina Nunes Rodrigues	* Rafael Martins - 14/0159461 <rafaelmartinsrm@gmail.com>
+* Nicholas Brant - 14/0157158
 */
-struct nodo{
-    struct nodo* prox;
-    struct nodo* ante;
-    char nome[15];
-    char genero;
-    char estado[10];
-    unsigned short int idade;
-}typedef Nodo;
 
-struct list{
-    Nodo* Atual;
-    Nodo* Primeiro;
-    Nodo* Ultimo;
-}typedef List;
+struct List
+{
+	Node* init; /* First element */
+	unsigned int n; /* Count */
+};
+typedef struct List List;
+
+struct Node
+{
+    struct Node* next;
+    struct Node* prev;
+    char name[15];
+    unsigned short int idade;
+    char gender; /* 0 if male, 1 if female */ 
+    char type; /* 0 if dwarf, 1 if mage */
+    unsigned short int humor; /* pessimo, ruim, moderado, bom, satisfeito, feliz */
+};
+typedef struct Node Node;
