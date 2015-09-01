cc8a5c89a85d99d00dfcd11b99ad2818b2b9bba2
nicbrant <nicbrant@gmail.com>
2015-08-31 23:49:10 -0300

    Ajuste do arquivo para a nova extrutura leitura e criação de nodos

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 #include"listas.h"
+void PegaDadosArquivo(List* atual){
+    char palavra[15],tipo,genero;
+    int idade,humor;
+    FILE* arquivo = fopen("Convidados.txt","r");
+    if(arquivo == NULL)
+        end(9);
+    while(fscanf(arquivo,"%s",palavra)!= EOF){
+            fscanf(arquivo,"%s %i %c %c %i",palavra,&idade,&genero,&tipo,&humor);
+            CriaNodo(atual,palavra,genero,tipo,humor,idade);
+    }
+    fclose(arquivo);
+}
+
 int main(){
     int i;
     List* mesa = CriaLista();
-    //
-    //criaçao dos nodos e elitura do arquivo
-    //
+    PegaDadosArquivo(mesa);
     Node* atual = mesa->init;
     while(ListaVazia(mesa)){
         if(atual->humor >= 5){
