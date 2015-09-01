0799f850aadd55b101039d4aae8143f5403d4bb7
Rafael Martins <rafaelmartinsrm@gmail.com>
2015-09-01 04:03:01 +0000

    Update main_teste.c

--- main_teste.c
+++ main_teste.c
@@ -5,24 +5,11 @@
 
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h" 
-#include "Exemplo.c"
+#include "main.c"
 /*
- * FunÃ§Ã£o que adiciona ao registro do CUnit os testes paras as funÃ§Ãµes 
- * presentes no arquivo testes.c
+ * Abaixo funções que fazem o teste.
  */
-void adicionar_testes_smo_datas(void);
-
-
-
-/*Abaixo estÃ£o as funÃ§Ãµes que efetuam os testes para a funÃ§Ã£o VerificaParidade*/
-void teste_DT_VerificaParidade_Par(void);
-void teste_DT_VerificaParidade_Impar(void);
-void teste_DT_VerificaParidade_Zero(void);
-
-/*Abaixo estÃ£o as funÃ§Ãµes que efetuam os testes para a funÃ§Ã£o ProximaLetra*/
-void teste_DT_ProximaLetra_Maiuscula(void);
-void teste_DT_ProximaLetra_Minuscula(void);
-void teste_DT_ProximaLetra_Z(void);
+void adicionar_testes_main(void); // Cria a suite
 
 
 /* Teste numero numero par */
@@ -32,65 +19,20 @@ void teste_DT_VerificaParidade_Par(void){
 	CU_ASSERT_TRUE( !resultado );
 }
 
-/* Teste numero impar */
-void teste_DT_VerificaParidade_Impar(void){
-	int resultado;
-	resultado = VerificaParidade(3);
-	CU_ASSERT_TRUE( resultado );
-}
-
-/* Teste numero zero */
-void teste_DT_VerificaParidade_Zero(void){
-	int resultado;
-	resultado = VerificaParidade(0);
-	CU_ASSERT_TRUE( !resultado );
-}
-
-/* Teste letra maiuscula */
-void teste_DT_ProximaLetra_Maiuscula(void){
-	char resultado;
-	resultado = ProximaLetra('A');
-	CU_ASSERT_TRUE( resultado == 'B' );
-}
-
-/* Teste letra maiuscula */
-void teste_DT_ProximaLetra_Minuscula(void){
-	char resultado;
-	resultado = ProximaLetra('d');
-	CU_ASSERT_TRUE( resultado == 'e' );
-}
-
-/* Teste letra maiuscula */
-void teste_DT_ProximaLetra_Z(void){
-	char resultado;
-	resultado = ProximaLetra('Z');
-	CU_ASSERT_TRUE( resultado == 'A' );
-}
-
-void  adicionar_testes_smo_datas(void){
+void  adicionar_testes_main(void){
 	CU_pSuite suite;
 	
 	/*Cria uma suite que conterÃ¡ todos os testes*/
-	suite = CU_add_suite("Testes da smo_datas",NULL,NULL);
+	suite = CU_add_suite("Testes da main",NULL,NULL);
 	
-	
-	/*Adiciona os testes para a funÃ§Ã£o DT_data_valida*/
-	CU_ADD_TEST(suite, teste_DT_VerificaParidade_Par);
-	CU_ADD_TEST(suite, teste_DT_VerificaParidade_Impar);
-	CU_ADD_TEST(suite, teste_DT_VerificaParidade_Zero);
-	CU_ADD_TEST(suite, teste_DT_ProximaLetra_Maiuscula);
-	CU_ADD_TEST(suite, teste_DT_ProximaLetra_Minuscula);
-	CU_ADD_TEST(suite, teste_DT_ProximaLetra_Z);
-
 }
-int
-main(void){
+int main(void){
 	/*Inicializa o registro de suÃ­tes e testes do CUnit*/	
 	if (CUE_SUCCESS != CU_initialize_registry())
     	return CU_get_error();
     
     /*Adiciona os testes ao registro*/ 	
-   	adicionar_testes_smo_datas();
+   	adicionar_testes_main();
 	/*Muda o modo do CUnit para o modo VERBOSE
 	 O modo VERBOSE mostra algumas informacoes a
 	 mais na hora da execucao*/
